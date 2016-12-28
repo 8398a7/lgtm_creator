55cc84929103d5c4317dbfd862d2ee8d3407ff9c
839 <8398a7@gmail.com>
2016-12-28 22:41:56 +0900

    [#2] add directory conversion function

--- lib/lgtm_creator/cli.rb
+++ lib/lgtm_creator/cli.rb
@@ -5,9 +5,19 @@ module LgtmCreator
   class CLI < Thor
     desc 'convert sample.png', 'add lgtm string.'
     option :r
-    def convert(src, dst)
+    def convert(src, dst = '')
       if options[:r]
+        Dir.glob(src + '/*').each do |filename|
+          next unless filename =~ /\.gif$/
+          paths = filename.split('/')
+          paths[paths.size - 1] = 'lgtm_' + paths.last
+          LgtmCreator::Core.convert(filename, paths.join('/'))
+        end
       else
+        if dst.empty?
+          puts 'Please specify a file name'
+          return
+        end
         LgtmCreator::Core.convert(src, dst)
       end
     end
